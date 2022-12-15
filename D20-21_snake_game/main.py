8690a64cba357f9117278e7ac532cf99eccd8e8b
Nicolas Draber <nicolas.draber@gmail.com>
2022-12-15 14:17:39 -0400

    Display the food

--- D20-21_snake_game/main.py
+++ D20-21_snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,10 +11,12 @@ screen.tracer(0)
 screen.listen()
 
 snake = Snake()
+food = Food()
 screen.onkey(fun=snake.up, key="Up")
 screen.onkey(fun=snake.down, key="Down")
 screen.onkey(fun=snake.right, key="Right")
 screen.onkey(fun=snake.left, key="Left")
+
 is_game_on = True
 while is_game_on:
     screen.update()
