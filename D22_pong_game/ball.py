94a5cdec3bdaab035eba792dbac98c868a2aed9c
Nicolas Draber <nicolas.draber@gmail.com>
2022-12-16 22:33:53 -0400

    Initial ball heading set between -30 and 30 deg

--- D22_pong_game/ball.py
+++ D22_pong_game/ball.py
@@ -1,23 +1,29 @@
 from turtle import Turtle
 import random
 
-BALL_SPEED = 3
+BALL_SPEED = 4
 
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("square")
         self.penup()
         self.color("white")
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.setheading(random.randint(-30, 30))
+        angle_start = random.randint(-30, 30)
+        self.tilt(-angle_start)
+        self.setheading(angle_start)
 
     def move(self):
         self.forward(BALL_SPEED)
 
     def bounce_top_bottom_walls(self):
-        self.setheading(-self.heading())
+        bounce_angle = -self.heading()
+        self.setheading(bounce_angle)
+        self.tiltangle(0)
 
     def bounce_left_right_walls(self):
-        self.setheading((180 - self.heading()) % 360)
+        bounce_angle = (180 - self.heading()) % 360
+        self.setheading(bounce_angle)
+        self.tiltangle(0)
