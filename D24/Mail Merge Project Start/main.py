4f6fbbb7b72ddbac574fca8b2aa9a78302c47a6b
Nicolas Draber <nicolas.draber@gmail.com>
2022-12-17 15:21:41 -0400

    Clean up comments

--- D24/Mail Merge Project Start/main.py
+++ D24/Mail Merge Project Start/main.py
@@ -8,18 +8,9 @@ with open("./Input/Letters/starting_letter.txt") as file:
 for name in names:
     name_stripped = name.strip()
     letter = generic_letter.replace("[name]", name_stripped)
-
+    # Write txt file
     with open(
         f"/Users/nicolas/git/100-days-of-Python/D24/Mail Merge Project Start/Output/ReadyToSend/letter_to_{name_stripped}.txt",
         mode="w",
     ) as file:
         file.write(letter)
-
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
