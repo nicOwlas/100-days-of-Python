ccc8eebb7ca653698cb5dfc4cd2afdbd5d05ab1a
Nicolas Draber <nicolas.draber@gmail.com>
2022-12-22 23:25:05 -0400

    Playground with arg kwargs tkinte

--- D27/main.py
+++ D27/main.py
@@ -1,5 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
+window.title("First GUI")
+window.minsize(width=600,height=400)
+
+# Labels
+my_label = Label(text="This is a test")
+my_label.pack(side="top")
+
+# Buttons
+def button_clicked():
+    print("Button got clicked")
+    my_label.config(text=entry.get())
+
+my_button = Button(text = "Click here!", command=button_clicked)
+my_button.pack()
+
+# Inputs
+entry = Entry()
+entry.pack()
+print()
+
 
 window.mainloop()
---
+++ D27/playground.py
@@ -0,0 +1,14 @@
+def add(*args):
+    return sum(args)
+
+print(add(1,2,3))
+
+ints = set([1,1,2,3,3,3,4])
+print(len(ints))
+
+def make_dict(
+*args
+):
+    return kwargs
+
+print(make_dict(a = 1, b = 2))
