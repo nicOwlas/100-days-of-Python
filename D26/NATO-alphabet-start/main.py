1aa07911093ba91c2caff087cfacae848fee0c19
Nicolas Draber <nicolas.draber@gmail.com>
2022-12-28 15:06:37 -0400

    Ask for new input if exception is raised

--- D26/NATO-alphabet-start/main.py
+++ D26/NATO-alphabet-start/main.py
@@ -6,11 +6,17 @@ student_data_frame = pandas.read_csv("./nato_phonetic_alphabet.csv")
 NATO_dic = {row.letter: row.code for (index, row) in student_data_frame.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Write a word. It will be spelled in the NATO dictionnary: ")
 
-try:
-    NATO_spelling = [NATO_dic[letter.upper()] for letter in word]
-except KeyError:
-    print("Only letters please")
-else:
-    print(NATO_spelling)
+
+def generate_nato_spelling():
+    word = input("Write a word. It will be spelled in the NATO dictionnary: ")
+    try:
+        NATO_spelling = [NATO_dic[letter.upper()] for letter in word]
+    except KeyError:
+        print("Only letters please")
+        generate_nato_spelling()
+    else:
+        print(NATO_spelling)
+
+
+generate_nato_spelling()
