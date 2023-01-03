dfba06075082a6ad589beadef78a11a8b0ca9459
Nicolas Draber <nicolas.draber@gmail.com>
2023-01-03 13:39:10 -0400

    Quizz app

---
+++ D34_quizz_app/D34_quizzler-app_start/data.py
@@ -0,0 +1,14 @@
+import requests
+import html
+
+
+def get_question_set(set_size):
+    """Return a set of questions from the trivia API"""
+    parameters = {"amount": set_size, "type": "boolean"}
+    response = requests.get(
+        url=f"https://opentdb.com/api.php", params=parameters, timeout=30
+    )
+    return response.json()["results"]
+
+
+question_data = get_question_set(10)
