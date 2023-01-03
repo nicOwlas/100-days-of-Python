e361c0f0e11cfc6e285aff89a162fc117b7d445c
Nicolas Draber <nicolas.draber@gmail.com>
2023-01-03 14:00:12 -0400

    Fix path to image

--- D28_pomodoro/pomodoro-start/main.py
+++ D28_pomodoro/pomodoro-start/main.py
@@ -1,5 +1,5 @@
-
 from tkinter import *
+
 
 def main():
     # ---------------------------- CONSTANTS ------------------------------- #
@@ -11,31 +11,33 @@ def main():
     WORK_MIN = {"duration": 25, "title": "WORK", "color": GREEN}
     SHORT_BREAK_MIN = {"duration": 5, "title": "BREAK", "color": PINK}
     LONG_BREAK_MIN = {"duration": 20, "title": "BREAK", "color": RED}
-    WORK_SERIE = [WORK_MIN,SHORT_BREAK_MIN]*4 + [LONG_BREAK_MIN]
+    WORK_SERIE = [WORK_MIN, SHORT_BREAK_MIN] * 4 + [LONG_BREAK_MIN]
     global task_index
     task_index = 0
     global timer
     timer = None
 
     def lift_window():
-        window.attributes('-topmost', True)
-        window.focus_force() 
-        window.attributes('-topmost', False)
-        #window.attributes('-topmost', 0)  
+        window.attributes("-topmost", True)
+        window.focus_force()
+        window.attributes("-topmost", False)
+        # window.attributes('-topmost', 0)
 
-    # ---------------------------- TIMER RESET ------------------------------- # 
+    # ---------------------------- TIMER RESET ------------------------------- #
 
     def reset_timer():
         window.after_cancel(timer)
-        check_marks.config(text="", fg= GREEN, bg=YELLOW)
-        timer_label.config(text="Timer",font=(FONT_NAME,60,"normal"),fg=GREEN, bg=YELLOW)
-        count_minutes = int(WORK_MIN["duration"]*60 // 60)
-        count_seconds = int(WORK_MIN["duration"]*60 % 60)
+        check_marks.config(text="", fg=GREEN, bg=YELLOW)
+        timer_label.config(
+            text="Timer", font=(FONT_NAME, 60, "normal"), fg=GREEN, bg=YELLOW
+        )
+        count_minutes = int(WORK_MIN["duration"] * 60 // 60)
+        count_seconds = int(WORK_MIN["duration"] * 60 % 60)
         canvas.itemconfig(timer_text, text=f"{count_minutes:02}:{count_seconds:02}")
         global task_index
         task_index = 0
 
-    # ---------------------------- TIMER MECHANISM ------------------------------- # 
+    # ---------------------------- TIMER MECHANISM ------------------------------- #
 
     def start_timer():
         global task_index
@@ -45,7 +47,7 @@ def main():
         timer_label.config(text=serie["title"], fg=serie["color"])
         timer_label.config(text=serie["title"], fg=serie["color"])
         task_index += 1
-        task_index %= len(WORK_SERIE) # Resume to initial task
+        task_index %= len(WORK_SERIE)  # Resume to initial task
 
     # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
     def count_down(count, serie):
@@ -56,10 +58,9 @@ def main():
             global timer
             timer = window.after(1000, count_down, count - 1, serie)
         else:
-            start_timer()        
-            if serie["title"] == "WORK":        
-                check_marks.config(text=check_marks.cget("text")+"✔")
-
+            start_timer()
+            if serie["title"] == "WORK":
+                check_marks.config(text=check_marks.cget("text") + "✔")
 
     # ---------------------------- UI SETUP ------------------------------- #
     window = Tk()
@@ -67,29 +68,53 @@ def main():
     window.config(padx=100, pady=50, bg=YELLOW)
 
     # Label
-    timer_label = Label(text="Timer",font=(FONT_NAME,60,"normal"),fg=GREEN, bg=YELLOW)
-    timer_label.grid(row=0,column=1)
+    timer_label = Label(
+        text="Timer", font=(FONT_NAME, 60, "normal"), fg=GREEN, bg=YELLOW
+    )
+    timer_label.grid(row=0, column=1)
 
     # Canvas
-    count_minutes = int(WORK_MIN["duration"]*60 // 60)
-    count_seconds = int(WORK_MIN["duration"]*60 % 60)
+    count_minutes = int(WORK_MIN["duration"] * 60 // 60)
+    count_seconds = int(WORK_MIN["duration"] * 60 % 60)
     canvas = Canvas(width=240, height=240, bg=YELLOW, highlightthickness=0)
-    tomato_image = PhotoImage(file="/Users/nicolas/git/100-days-of-Python/D28/pomodoro-start/tomato.png")
-    canvas.create_image(120,120,image = tomato_image)
-    timer_text = canvas.create_text(120, 145, text=f"{count_minutes:02}:{count_seconds:02}", font=(FONT_NAME,35,"bold"), fill="white")
-    canvas.grid(row=1,column=1)
+    # Absolute path as packaging with pants does not work with relative paths unfortunately
+    tomato_image = PhotoImage(
+        file="/Users/nicolas/git/100-days-of-Python/D28_pomodoro/pomodoro-start/tomato.png"
+    )
+    canvas.create_image(120, 120, image=tomato_image)
+    timer_text = canvas.create_text(
+        120,
+        145,
+        text=f"{count_minutes:02}:{count_seconds:02}",
+        font=(FONT_NAME, 35, "bold"),
+        fill="white",
+    )
+    canvas.grid(row=1, column=1)
 
     # Buttons
-    start_button = Button(text="Start", highlightthickness=0, bg=YELLOW, borderwidth=0, command=start_timer)
+    start_button = Button(
+        text="Start",
+        highlightthickness=0,
+        bg=YELLOW,
+        borderwidth=0,
+        command=start_timer,
+    )
     start_button.grid(row=2, column=0)
 
-    reset_button = Button(text="Reset",  highlightthickness=0, bg=YELLOW, borderwidth=0, command=reset_timer)
+    reset_button = Button(
+        text="Reset",
+        highlightthickness=0,
+        bg=YELLOW,
+        borderwidth=0,
+        command=reset_timer,
+    )
     reset_button.grid(row=2, column=2)
 
-    check_marks = Label(text="", fg= GREEN, bg=YELLOW)
+    check_marks = Label(text="", fg=GREEN, bg=YELLOW)
     check_marks.grid(row=2, column=1)
 
     window.mainloop()
+
 
 if __name__ == main():
     main()
