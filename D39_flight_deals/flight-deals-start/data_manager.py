b7572388d2213e4bfe19bb87b2ede6bc3d1fd693
Nicolas Draber <nicolas.draber@gmail.com>
2023-01-10 10:01:59 -0400

    Transform data_manager into a class

--- D39_flight_deals/flight-deals-start/data_manager.py
+++ D39_flight_deals/flight-deals-start/data_manager.py
@@ -1,6 +1,4 @@
-# class DataManager:
-#     #This class is responsible for talking to the Google Sheet.
-#     pass
+"""This class is responsible for talking to the Google Sheet."""
 import os
 
 import inflect
@@ -11,12 +9,8 @@ p = inflect.engine()
 
 load_dotenv()
 
-username = os.environ.get("SHEETY_USERNAME")
-token = os.environ.get("SHEETY_TOKEN")
-header = {"Authorization": f"Bearer {token}"}
 
-
-def convert_to_camel_case(text: str) -> str:
+def to_camel_case(text: str) -> str:
     """Returns a string in camelCase"""
     text_spaced = text.replace("-", " ").replace("_", " ")
     text_split = text_spaced.split()
@@ -27,36 +21,46 @@ def convert_to_camel_case(text: str) -> str:
     )
 
 
-def get_sheet_data(file_name: str, sheet_name: str) -> dict:
-    """Read google sheet values and return its values"""
-    file_name_formatted = convert_to_camel_case(file_name)
-    sheet_name_formatted = convert_to_camel_case(sheet_name)
-    api_endpoint = (
-        f"https://api.sheety.co/{username}/{file_name_formatted}/{sheet_name_formatted}"
-    )
-    response = requests.get(url=api_endpoint, headers=header, timeout=30)
-    print(response.json())
-    return response.json().get(sheet_name)
+class DataManager:
+    """This class is responsible for talking to the Google Sheet"""
 
+    def __init__(self, file_name: str, tab_name: str) -> None:
+        self.username = os.environ.get("SHEETY_USERNAME")
+        self.file_name = to_camel_case(file_name)
+        self.tab_name = to_camel_case(tab_name)
+        self.token = os.environ.get("SHEETY_TOKEN")
+        self.header = {"Authorization": f"Bearer {self.token}"}
+        self.api_endpoint = (
+            f"https://api.sheety.co/{self.username}/{self.file_name}/{self.tab_name}"
+        )
+        # Return the sheet name if its singular does not exist (prices -> price)
+        self.tab_name_singular = (
+            tab_name if not p.singular_noun(tab_name) else p.singular_noun(tab_name)
+        )
 
-def update_row(file_name: str, sheet_name: str, row_value: dict) -> None:
-    """Update a given row. dictionnary with key/value.
-    Keys : camelCase of the column titles / Values: data to be updated
-    An 'id' key takes an int for value. It's the row index (starts at 1) to be updated
-    """
-    file_name_formatted = convert_to_camel_case(file_name)
-    sheet_name_formatted = convert_to_camel_case(sheet_name)
-    row_id = row_value.get("id")
-    api_endpoint = f"https://api.sheety.co/{username}/{file_name_formatted}/{sheet_name_formatted}/{row_id}"
+    def get_tab_data(self) -> dict:
+        """Read and return values stored in tab_name"""
+        response = requests.get(url=self.api_endpoint, headers=self.header, timeout=30)
+        return response.json().get(self.tab_name)
 
-    # SHEETY requires the row_data to be in a dict having as a key the singular of the sheet_name...
-    payload = {p.singular_noun(sheet_name): row_value}
-    requests.put(url=api_endpoint, json=payload, headers=header, timeout=30)
+    def update_row(self, row_data: dict) -> None:
+        """Update a given row. dictionnary with key/value.
+        Keys : camelCase of the column titles / Values: data to be updated
+        An 'id' key takes an int for value. It's the row index (starts at 1) to be updated
+        """
+        row_id = row_data.get("id")
+        update_row_endpoint = f"{self.api_endpoint}/{row_id}"
+
+        payload = {self.tab_name_singular: row_data}
+        requests.put(
+            url=update_row_endpoint, json=payload, headers=self.header, timeout=30
+        )
 
 
 if __name__ == "__main__":
-    google_sheet_name = "Flight Deals"
-    tab_name = "prices"
-    row_value = {"city": "Paris", "iataCode": "", "lowestPrice": 54, "id": 2}
-    get_sheet_data(google_sheet_name, tab_name)
-    update_row(google_sheet_name, tab_name, row_value)
+    FILE_NAME = "Flight Deals"
+    TAB_NAME = "prices"
+    row = {"city": "Paris", "iataCode": "", "lowestPrice": 54, "id": 2}
+    data_manager = DataManager(file_name=FILE_NAME, tab_name=TAB_NAME)
+    data_manager.get_tab_data()
+    data_manager.update_row(row_data=row)
--- D39_flight_deals/flight-deals-start/main.py
+++ D39_flight_deals/flight-deals-start/main.py
@@ -1,25 +1,41 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from data_manager import get_sheet_data, update_row
-from flight_search import get_iata_city_code
+from datetime import date, datetime, timedelta
+
+from data_manager import DataManager
+from flight_search import flight_search, get_iata_city_code
 
 FILE_NAME = "Flight Deals"
 TAB_NAME = "prices"
-FLY_FROM_IATA_CODE = "PAR"
+ORIGN_CITY = "PARIS"
 
 # Read the list of destinations
-google_sheet_name = "Flight Deals"
-tab_name = "prices"
-destinations = get_sheet_data(FILE_NAME, TAB_NAME)
+FILE_NAME = "Flight Deals"
+TAB_NAME = "prices"
+data_manager = DataManager(file_name=FILE_NAME, tab_name=TAB_NAME)
+destinations = data_manager.get_tab_data()
+
+origin_city_iata_code = get_iata_city_code(ORIGN_CITY)
 
 for destination in destinations:
+    destination_city_iata_code = get_iata_city_code(destination.get("city"))
     row_value = {
-        "iataCode": get_iata_city_code(destination.get("city")),
+        "iataCode": destination_city_iata_code,
         "id": destination.get("id"),
     }
     # Update the city IATA code
-    update_row(FILE_NAME, TAB_NAME, row_value)
+    data_manager.update_row(row_value)
 
     # TODO: For each destination, search flights date_from = tomorrow, date_to = tomorrow + 180
+    tomorrow = date.today() + timedelta(days=1)
+    tomorrow_plus_six_months = tomorrow + timedelta(days=180)
+    print(f"{tomorrow}\n{tomorrow_plus_six_months}")
+
+    # flight_search(
+    #     fly_from=origin_city_iata_code,
+    #     fly_to=destination_city_iata_code,
+    #     date_from=tomorrow.isoformat(),
+    #     date_to=tomorrow_plus_six_months.isoformat(),
+    # )
 
 # TODO: For each search result, find the min price
 # TODO: If min_price less than present_price, update google sheet
