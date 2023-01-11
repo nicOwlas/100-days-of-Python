10191a6e371e5d3459c851bd1896b0e2b74d7ac4
Nicolas Draber <nicolas.draber@gmail.com>
2023-01-11 15:01:11 -0400

    Concurrency at every level

--- D39_flight_deals/flight-deals-start/main.py
+++ D39_flight_deals/flight-deals-start/main.py
@@ -1,68 +1,113 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from datetime import date, datetime, timedelta
+import asyncio
+from datetime import date, timedelta
 
 from data_manager import DataManager
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
 
-def cheap_flight_scanner(
-    file_name: str, tab_name: str, origin_city: str, email_to: str
+async def cheap_flight_scanner(
+    file_name: str, tab_name: str, departure_city: str, email_to: str
 ) -> None:
     """Scan for cheapest flights"""
     data_manager = DataManager(file_name=file_name, tab_name=tab_name)
     flight_search = FlightSearch()
     notification_manager = NotificationManager()
 
-    destinations = data_manager.get_tab_data()
-    origin_city_iata_code = flight_search.get_iata_city_code(origin_city)
+    destinations = await data_manager.get_tab_data()
+    departure_iata_city_code = await flight_search.get_iata_city_code(departure_city)
 
-    for destination in destinations:
-        print(f"Scanning flights to {destination.get('city')}")
-        destination_city_iata_code = flight_search.get_iata_city_code(
-            destination.get("city")
-        )
-        row_value = {
-            "iataCode": destination_city_iata_code,
-            "id": destination.get("id"),
-        }
-        # Update the city IATA code
-        data_manager.update_row(row_value)
+    # Get city IATA codes
+    async with asyncio.TaskGroup() as tg:
+        iata_codes = [
+            tg.create_task(flight_search.get_iata_city_code(destination.get("city")))
+            for destination in destinations
+        ]
 
-        # TODO: For each destination, search flights date_from = tomorrow, date_to = tomorrow + 180
-        tomorrow = date.today() + timedelta(days=1)
-        tomorrow_plus_six_months = tomorrow + timedelta(days=180)
-        # print(f"{tomorrow}\n{tomorrow_plus_six_months}")
+    # Update the dictionnary
+    destinations = [
+        dict(destination, iataCode=iata_codes[index].result())
+        for (index, destination) in enumerate(destinations)
+    ]
 
-        flights = flight_search.search(
-            fly_from=origin_city_iata_code,
-            fly_to=destination_city_iata_code,
-            date_from=tomorrow.isoformat(),
-            date_to=tomorrow_plus_six_months.isoformat(),
-            nights_in_dst_from=destination.get("minNights", 1),
-            nights_in_dst_to=destination.get("maxNights", 1),
-        )
+    # Update the Google Sheet with the codes
+    async with asyncio.TaskGroup() as tg:
+        _ = [
+            tg.create_task(
+                data_manager.update_row(
+                    {
+                        "iataCode": destination.get("iataCode"),
+                        "id": destination.get("id"),
+                    }
+                )
+            )
+            for destination in destinations
+        ]
 
-        # If min_price less than present_price, update google sheet
+    tomorrow = date.today() + timedelta(days=1)
+    tomorrow_plus_six_months = tomorrow + timedelta(days=180)
+
+    # Search flights to destination
+    async with asyncio.TaskGroup() as tg:
+        searches = [
+            tg.create_task(
+                flight_search.search(
+                    fly_from=departure_iata_city_code,
+                    fly_to=destination.get("iataCode"),
+                    date_from=tomorrow.isoformat(),
+                    date_to=tomorrow_plus_six_months.isoformat(),
+                    nights_in_dst_from=destination.get("minNights", 1),
+                    nights_in_dst_to=destination.get("maxNights", 1),
+                )
+            )
+            for destination in destinations
+        ]
+
+    tab_data = []
+    emails_text = []
+    for index, search in enumerate(searches):
+        print(f"Scanning flights to {destinations[index].get('city')}")
+        flights = search.result()
+
         if len(flights) > 0:
             cheapest_flight = flights[0]
-            if cheapest_flight.get("price") < destination.get("lowestPrice"):
+            if cheapest_flight.get("price") < destinations[index].get(
+                "lowestPrice", 1000
+            ):
                 departure_date = cheapest_flight.get("route")[0].get("local_departure")
                 return_date = cheapest_flight.get("route")[-1].get("local_arrival")
 
-                # Send email
-                email_text = notification_manager.format_email(cheapest_flight)
-                notification_manager.send_email(
-                    recipient_email=email_to,
-                    subject=email_text.get("subject"),
-                    message=email_text.get("message"),
+                # Store text email
+                emails_text.append(notification_manager.format_email(cheapest_flight))
+
+                # Store flight details
+                tab_data.append(
+                    {
+                        "id": destinations[index].get("id"),
+                        "lowestPrice": cheapest_flight.get("price"),
+                        "departureDate": departure_date,
+                        "returnDate": return_date,
+                    }
                 )
 
-                # Update google sheet
-                row_value["lowestPrice"] = cheapest_flight.get("price")
-                row_value["departureDate"] = departure_date
-                row_value["returnDate"] = return_date
-                data_manager.update_row(row_value)
+    # Send emails & update google sheet
+    async with asyncio.TaskGroup() as tg:
+        _ = [
+            tg.create_task(
+                notification_manager.send_async_email(
+                    recipient_email=email_to,
+                    subject=email_text.get("subject"),
+                    body=email_text.get("message"),
+                )
+            )
+            for email_text in emails_text
+        ]
+
+        # Update google sheet with flights data
+        _ = [
+            tg.create_task(data_manager.update_row(row_value)) for row_value in tab_data
+        ]
 
 
 if __name__ == "__main__":
@@ -73,12 +118,15 @@ if __name__ == "__main__":
     load_dotenv()
     FILE_NAME = "Flight Deals"
     TAB_NAME = "prices"
-    ORIGN_CITY = "PARIS"
+    ORIGN_CITY = "Pointe à pitre"
     EMAIL_TO = os.environ.get("EMAIL_RECIPIENT")
 
-    cheap_flight_scanner(
-        file_name=FILE_NAME,
-        tab_name=TAB_NAME,
-        origin_city=ORIGN_CITY,
-        email_to=EMAIL_TO,
-    )
+    async def main():
+        await cheap_flight_scanner(
+            file_name=FILE_NAME,
+            tab_name=TAB_NAME,
+            departure_city=ORIGN_CITY.replace(" ", "-"),
+            email_to=EMAIL_TO,
+        )
+
+    asyncio.run(main())
